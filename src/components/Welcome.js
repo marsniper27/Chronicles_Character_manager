0b9355deac27eef82cc8c63d4f2900d8fca5d4c5
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2022-08-08 00:21:19 -0300

    set up for fleek

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,14 +12,14 @@ const DiscordOauth2 = require("discord-oauth2");
 const oauth = new DiscordOauth2({
   clientId: '975582303734607872',
   clientSecret: '6m8r3KkaP3xiwPVIk4iLDMep-HcAD6F5',
-  redirectUri: 'http://localhost:3000',
+  redirectUri: 'https://gentle-tooth-0749.on.fleek.co/',
 });
 
 export function Welcome() {
 
  const [discordName, setDiscordName] = useState("");
 
-    if(discordName === "" ){
+    if(discordName === "" & window.location.search !== ""){
         return (
             <Container>
                 <h1 >Gargoyles</h1>
@@ -38,6 +38,18 @@ export function Welcome() {
                         }).catch(console.error);
                     }}}>
                 Get Discord Account
+                </button>
+            </Container>
+        )
+    }
+    else if(window.location.search === ""){
+        return (
+            <Container>
+                <h1 >Gargoyles</h1>
+                <h1>Welcome</h1>
+                
+                <button onClick={() => {window.open("https://discord.com/oauth2/authorize?client_id=975582303734607872&redirect_uri=https%3A%2F%2Fgentle-tooth-0749.on.fleek.co%2F&response_type=code&scope=identify", '_self', 'noopener,noreferrer');}}>
+                Authorize Discord
                 </button>
             </Container>
         )
