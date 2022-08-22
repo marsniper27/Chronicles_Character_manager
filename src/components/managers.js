4a383fa1ebf156b69102cb94da998b8ffe2a1831
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2022-08-21 21:18:22 -0300

    added tzi

--- src/components/managers.js
+++ src/components/managers.js
@@ -1,5 +1,6 @@
 export const managers = [
-    "F1p5ct9NqBP63Zrf2QBFn1MAGTNNiRG6BRkmm4g5vCUS"
+    "F1p5ct9NqBP63Zrf2QBFn1MAGTNNiRG6BRkmm4g5vCUS",
+    "7VWW4NoGy7mUmbRRTdwdji6czWwAZxeTgRr7hULQfBBr"
 ]
 
 export default managers
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 require("./styles/index.css");
@@ -8,9 +8,9 @@ require("@solana/wallet-adapter-react-ui/styles.css");
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />  
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
